83482b190c6c5f4258d5a2feafc61bde799beb6d
Gogo553 <84742670+Gogo553@users.noreply.github.com>
2022-02-05 13:46:40 +0100

    Create github-actions-scripts.sh

---
+++ github-actions-scripts.sh
@@ -0,0 +1,13 @@
+  echo What is in this directory?
+          ls -a 
+          echo
+          echo Is Java installed?
+          java -version
+          echo
+          echo Is Git installed?
+          git --version 
+          echo 
+          echo What about build tools?
+          mvn --version
+          gradle --version
+          ant -version
